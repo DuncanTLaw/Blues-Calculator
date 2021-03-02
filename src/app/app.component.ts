69ed80fc11ff22017b7365e72d49d621831bd6f0
Duncan Law <lawdtk@gmail.com>
2021-03-02 18:46:44 +0000

    Add goal blues calc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { getLocalePluralCase } from '@angular/common';
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
@@ -7,13 +8,17 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  blueAchieved: string = '';
-  ipfPoints: number = 0;
   male: boolean = true;
   c1: number = 0;
   c2: number = 0;
   c3: number = 0;
   c4: number = 0;
+
+  blueAchieved: string = '';
+  ipfPoints: number = 0;
+
+  goalIPF: number = 0;
+  goalTotal: number = 0;
 
   calcBlue(form: NgForm): void {
     this.male = (form.value.gender === 'M') ? true : false;
@@ -41,6 +46,27 @@ export class AppComponent {
         this.blueAchieved = 'Full Blue';
       }
     }
-    console.log(this.ipfPoints)
+  }
+
+  calcGoal(form: NgForm): void {
+    this.male = (form.value.gender === 'M') ? true : false;
+    if (this.male) {
+      this.c1 = 310.67;
+      this.c2 = 857.785;
+      this.c3 = 53.216;
+      this.c4 = 147.0835;
+    } else {
+      this.c1 = 125.1435;
+      this.c2 = 228.03;
+      this.c3 = 34.5246;
+      this.c4 = 86.8301;
+    }
+    if (form.value.goalBlue === 'full') {
+      this.goalIPF = 560;
+    } else {
+      this.goalIPF = 500;
+    }
+    this.goalTotal = ((this.goalIPF - 500) / 100) * (this.c3 * Math.log(form.value.weight) - this.c4) +
+      (this.c1 * Math.log(form.value.weight) - this.c2);
   }
 }
