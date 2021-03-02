d2f09cd4ceb6f81fd563c0f0f8b002b57556c2ce
Duncan Law <lawdtk@gmail.com>
2021-03-02 19:46:04 +0000

    Group coefficient change into function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,19 +7,19 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  male: boolean = true;
-  c1: number = 0;
-  c2: number = 0;
-  c3: number = 0;
-  c4: number = 0;
+  male: boolean;
+  c1: number;
+  c2: number;
+  c3: number;
+  c4: number;
 
-  blueAchieved: string = '';
-  ipfPoints: number = 0;
+  blueAchieved: string;
+  ipfPoints: number;
 
-  goalIPF: number = 0;
-  goalTotal: number = 0;
+  goalIPF: number;
+  goalTotal: number;
 
-  calcBlue(form: NgForm): void {
+  coeffChange(form: NgForm): void {
     this.male = (form.value.gender === 'M') ? true : false;
     if (this.male) {
       this.c1 = 310.67;
@@ -32,6 +32,10 @@ export class AppComponent {
       this.c3 = 34.5246;
       this.c4 = 86.8301;
     }
+  }
+
+  calcBlue(form: NgForm): void {
+    this.coeffChange(form);
     this.ipfPoints = 500 + 100 * (
       (form.value.total - (this.c1 * Math.log(form.value.weight) - this.c2)) /
       (this.c3 * Math.log(form.value.weight) - this.c4)
@@ -48,18 +52,7 @@ export class AppComponent {
   }
 
   calcGoal(form: NgForm): void {
-    this.male = (form.value.gender === 'M') ? true : false;
-    if (this.male) {
-      this.c1 = 310.67;
-      this.c2 = 857.785;
-      this.c3 = 53.216;
-      this.c4 = 147.0835;
-    } else {
-      this.c1 = 125.1435;
-      this.c2 = 228.03;
-      this.c3 = 34.5246;
-      this.c4 = 86.8301;
-    }
+    this.coeffChange(form);
     if (form.value.goalBlue === 'full') {
       this.goalIPF = 560;
     } else {
